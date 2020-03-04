1fdc3949b80e2bcf6bceeee8e16ec5f44344a075
Darrel Idiagbor <darrel@farmstripe.com>
2020-03-04 12:35:23 +0100

    chore: prepare build for deplyoment

---
+++ build/precache-manifest.2373009c067c06a1f3f6efd4263922b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da2f7ee24a7f925df3cb9e4cdd30fc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4123b1b3e689e4bc5444",
+    "url": "/static/css/2.091bdaf9.chunk.css"
+  },
+  {
+    "revision": "a1b984e858efc9fea77c",
+    "url": "/static/css/main.00391da0.chunk.css"
+  },
+  {
+    "revision": "4123b1b3e689e4bc5444",
+    "url": "/static/js/2.34e9702e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.34e9702e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b984e858efc9fea77c",
+    "url": "/static/js/main.7a82cc1c.chunk.js"
+  },
+  {
+    "revision": "77b8dad9641a813b46f8",
+    "url": "/static/js/runtime-main.ea6d0dd5.js"
+  }
+]);
